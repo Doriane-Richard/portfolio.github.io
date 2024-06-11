8e086915daf15fa2b2d5c757c333a1a33f67d6b0
Doriane-Richard <doriane.richard08@gmail.com>
2024-06-11 15:29:24 +0200

    Intégrations des pages : contact, projet professionel et projet personnel.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,23 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const appDiv = document.getElementById('pageAccueil');
-  
-    fetch('html/accueil.html')
-      .then(response => response.text())
-      .then(data => {
-        appDiv.innerHTML = data;
-      })
-      .catch(error => {
-        console.error('Erreur lors du chargement du contenu:', error);
-        appDiv.innerHTML = '<p>Une erreur s\'est produite lors du chargement du contenu.</p>';
-      });
+function appelerPage(nomPageHtml, nomElementHtml) {
+  fetch('html/${nomPageHtml}')
+  .then(response => response.text())
+  .then(data => {
+    const element = document.getElementById(nomElementHtml);
+    element.innerHTML = data;
+  })
+  .catch(error => {
+    console.error('Erreur lors du chargement du contenu:', error);
+    appDiv.innerHTML = '<p>Une erreur s\'est produite lors du chargement du contenu.</p>';
   });
-  
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const pageContact = document.getElementById('pageContact');
+  const pageProjetProfessionnel = document.getElementById('pageProjetProfessionnel');
+  const pageProjetPersonnel = document.getElementById('pageProjetPersonnel');
+
+  appelerPage("contact.html", pageContact);
+  appelerPage("pageProjetProfessionnel.html", pageProjetProfessionnel);
+  appelerPage("pageProjetPersonnel.html", pageProjetPersonnel);
+});
+
