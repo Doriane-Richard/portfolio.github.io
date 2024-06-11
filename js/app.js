fb3d786330893a586b94b199bc3814962c56b036
Doriane-Richard <doriane.richard08@gmail.com>
2024-06-11 17:43:30 +0200

    Ajout d'un menu de navigation.

--- js/app.js
+++ js/app.js
@@ -19,21 +19,3 @@ document.addEventListener('DOMContentLoaded', () => {
   appelerPage("projetProfessionnel.html", pageProjetProfessionnel);
   appelerPage("projetPersonnel.html", pageProjetPersonnel);
 });
-
-function envoyerEmail(nom, email, message) {
-  console.log(`Le nom de l'expéditeur est : ${nom}, son adresse mail est : ${email} et son message est : ${message}`)
-
-  Email.send({
-    Host: "smtp.gmail.com",
-    Username: "doriane.richard.portfolio@gmail.com",
-    Password: "IlNeFautPasLireLeMDPDesAutres1234",
-    To: "doriane.richard.portfolio@gmail.com",
-    From: email,
-    Subject: `Nouveau message de ${nom} via le formulaire de contact`,
-    Body: message
-  }).then(
-    message => alert("Email envoyé avec succès!")
-  ).catch(error => {
-    console.error('Erreur lors de l\' envoi du mail :', error);
-  });;
-}
